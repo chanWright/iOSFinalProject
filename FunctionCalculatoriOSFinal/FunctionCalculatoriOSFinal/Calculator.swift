87d114a097766d23cb86d21856ef274a13cf9866
ganesh58 <srpganesh10@gmail.com>
2019-10-24 22:47:12 -0500

    modified names

--- FunctionCalculatoriOSFinal/FunctionCalculatoriOSFinal/Calculator.swift
+++ FunctionCalculatoriOSFinal/FunctionCalculatoriOSFinal/Calculator.swift
@@ -38,8 +38,8 @@ class Calculator{
     
     init(){
         addGroup(group: Groups(groupName: "Maths", functions: [Functions(functionName: "Pythagorean Theorem", formula:"A^2 + B^2 = c^2", variables: ["a","b","c"]),Functions(functionName: "Subtract", formula: " ", variables: []),Functions(functionName: "Minus", formula: " ", variables: []),Functions(functionName: "Division", formula: " ", variables: [])]))
-        addGroup(group: Groups(groupName: "Physics", functions: [Functions(functionName: "Einstein", formula: " ", variables: []),Functions(functionName: "Franklin", formula: " ", variables: []),Functions(functionName: "Grahambell", formula: " ", variables: [])]))
-        addGroup(group: Groups(groupName: "Chemistry", functions: [Functions(functionName: "H20", formula: " ", variables: []),Functions(functionName: "Nacl",formula: " ", variables: []),Functions(functionName: "K20",formula: " ", variables: []),Functions(functionName: "Li",formula: " ", variables: [])]))
+        addGroup(group: Groups(groupName: "Physics", functions: [Functions(functionName: "Force", formula: "mass * accleration = Force", variables: ["mass", "acceleration"]),Functions(functionName: "Speed", formula: "Distance/Time", variables: ["distance","time"]),Functions(functionName: "Kinetic Energy", formula: "(mass * velocity)/2", variables: ["mass","velocity"])]))
+        addGroup(group: Groups(groupName: "Chemistry", functions: [Functions(functionName: "Atomic Number", formula: "number of protons + number of nuetrons", variables: ["protons","neutrons"]),Functions(functionName: "Mass Number",formula: "Number of protons + number of Neutrons", variables: ["protons", "neutrons"]),Functions(functionName: "K20",formula: " ", variables: []),Functions(functionName: "Li",formula: " ", variables: [])]))
         addGroup(group: Groups(groupName: "Mischellenous", functions: [Functions(functionName: "M", formula: " ", variables: []),Functions(functionName: "N", formula: " ", variables: [])]))
     }
 }
